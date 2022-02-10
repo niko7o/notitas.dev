9f2b95d5279f18e55e4f3503fa5632582f5eb457
Nikolaas Verlee <nikotomad@gmail.com>
2022-02-10 23:45:55 +0100

    added next/head with some metadata

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
-// import Head from 'next/head'
+import Head from 'next/head'
 
 import TodoList from '../components/TodoList';
 
-export default function Home() {
-  return (
+const Home = () => (
+  <>
+    <Head>
+      <title>notitas.dev</title>
+      <meta name="description" content="Tus notitas en todas partes" />
+      <meta name="keywords" content="notitas, notitas.dev, notes, notas" />
+      <meta property="og:title" content="notitas.dev" key="title" />
+      <meta property="og:description" content="Tus notitas en todas partes" key="description" />
+      <meta name="author" content="nikoto" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
     <TodoList />
-  )
-}
+  </>
+)
+
+export default Home;
