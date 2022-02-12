7a55bae4481a6db485aae332d2895b515b6e4389
Nikolaas Verlee <nikotomad@gmail.com>
2022-02-12 01:50:35 +0100

    Added meaningful h1 for SEO + style changes

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -7,17 +7,20 @@ import styles from './Hero.module.scss';
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div className={styles['hero-list']}>
+      <div className={styles['hero-left']}>
         <TodoList />
       </div>
-      <div className={styles['hero-illustration']}>
-        <Image
-          src="/notes.svg"
-          width={600}
-          height={400}
-          quality={100}
-          priority 
-        />
+      <div className={styles['hero-right']}>
+        <h1>Guarda notitas a la velocidad de la luz.</h1>
+        <div className={styles['hero-image']}>
+          <Image
+            src="/notes.svg"
+            width={600}
+            height={400}
+            quality={100}
+            priority 
+          />
+        </div>
       </div>
     </div>
   )
