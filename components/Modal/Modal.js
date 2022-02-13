c87dbbb20933f6cc4d77ceee96366bcf2a1c19a6
Nikolaas Verlee <nikotomad@gmail.com>
2022-02-13 16:57:32 +0100

    new animated Modal component

---
+++ components/Modal/Modal.js
@@ -0,0 +1,58 @@
+import { useRef, useEffect, useState } from 'react';
+
+import { motion } from 'framer-motion';
+
+import { useMousePosition } from '../../utils/hooks';
+
+import { entering, idle, exiting, customTransition } from './animations';
+
+import styles from './Modal.module.scss'
+
+const Modal = ({ children, closeModal }) => {
+  const modalRef = useRef(null);
+  const { mousePosition } = useMousePosition();
+  const { x: cursorX, y: cursorY } = mousePosition;
+
+  const handleClickOutside = event => {
+    if (!modalRef.current.contains(event.target)) {
+      closeModal();
+      document.body.style.cursor = 'default';
+    }
+  }
+
+  useEffect(() => {
+    const modalBounds = modalRef.current.getBoundingClientRect();
+    const isCursorOutOfModalBounds =
+      // extra check to ensure modal is mounted here 
+      cursorX > modalBounds.right 
+      || cursorX < modalBounds.left
+      || cursorY < modalBounds.top
+      || cursorY > modalBounds.bottom;
+
+    if (isCursorOutOfModalBounds) {
+      document.body.style.cursor = 'pointer';
+    } else {
+      document.body.style.cursor = 'default';
+    }
+  }, [modalRef, mousePosition])
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [])
+
+  return (
+    <motion.div 
+      className={styles.modal}
+      initial={entering}
+      animate={idle}
+      exit={exiting}
+      transition={customTransition}
+      ref={modalRef}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+export default Modal;
---
+++ components/Modal/animations.js
@@ -0,0 +1,21 @@
+
+export const entering = { 
+  scale: .80,
+  y: '-100vh' 
+}
+
+export const idle = { 
+  scale: 1,
+  x: 0,
+  y: 0,
+}
+
+export const exiting = {
+  scale: .80,
+  y: '130vh',
+}
+
+export const customTransition = {
+  type: 'spring',
+  damping: 12
+}
