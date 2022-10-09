035a72facb7053ffcbbefe5e1c63117735e27e60
Pol Gubau <gubaupol@gmail.com>
2022-10-09 11:17:38 +0200

    Update notes.js

--- pages/api/notes.js
+++ pages/api/notes.js
@@ -1,16 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).send([
+  const arrayDefault = [
     {
-      id: `Kodak ColorPlus${Date.now()}`,
-      title: 'Kodak ColorPlus',
-      isCompleted: false
+      id: `0`,
+      title: "Kodak ColorPlus",
+      description:
+        "Kodak ColorPlus 200 is a ISO 200, daylight-balanced, color negative film with a fine grain structure and a wide exposure latitude. It is a good choice for general photography, especially in bright light.",
+      creationDate: new Date(),
+      isCompleted: false,
     },
     {
-      id: `Fujifilm CK40${Date.now()}`,
-      title: 'Fujifilm CK40',
-      isCompleted: true
-    }]
-  )
+      id: `1`,
+      title: "Kodak ColorPlus 2",
+      description:
+        "Kodak ColorPlus 200 is a ISO 200, daylight-balanced, color negative film with a fine grain structure and a wide exposure latitude. It is a good choice for general photography, especially in bright light.",
+      creationDate: new Date(),
+      isCompleted: false,
+    },
+    {
+      id: `2`,
+      title: "Kodak ColorPlus 3",
+      description:
+        "Kodak ColorPlus 200 is a ISO 200, daylight-balanced, color negative film with a fine grain structure and a wide exposure latitude. It is a good choice for general photography, especially in bright light.",
+      creationDate: new Date(),
+      isCompleted: false,
+    },
+  ];
+
+  res.status(200).json(arrayDefault);
 }
