fae32035213783e54829ad1faafb77e250f5a38a
Pol Gubau <gubaupol@gmail.com>
2022-10-09 11:16:56 +0200

    Update animations.js

--- components/TodoList/animations.js
+++ components/TodoList/animations.js
@@ -1,18 +1,18 @@
 export const containerVariants = {
   active: {
     transition: {
-      staggerChildren: 0.5
-    }
-  }
-}
+      staggerChildren: 0.5,
+    },
+  },
+};
 
 export const itemVariants = {
   entering: {
     y: -60,
-    opacity: 0
+    opacity: 0,
   },
   active: {
     y: 0,
-    opacity: 1
+    opacity: 1,
   },
-}
+};
