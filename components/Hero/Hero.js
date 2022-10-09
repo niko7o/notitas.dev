5f5c8eed26fac54d2d7bd34bef778991b43ac144
Pol Gubau <gubaupol@gmail.com>
2022-10-09 11:18:29 +0200

    Update Hero.js

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,58 +1,58 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence } from "framer-motion";
 
-import Author from '../Author';
-import TodoList from '../TodoList';
-import Modal from '../Modal';
-import HowItWorks from '../HowItWorks';
+import Author from "../Author";
+import TodoList from "../TodoList";
+import Modal from "../Modal";
+import HowItWorks from "../HowItWorks";
 
-import styles from './Hero.module.scss';
+import styles from "./Hero.module.scss";
 
-import { useModalHandle } from '../../utils/hooks';
-import { LOCAL_STORAGE_KEY } from '../../utils/constants';
+import { LOCAL_STORAGE_KEY } from "../../utils/constants";
+import { useState } from "react";
 
 const Hero = () => {
-  const { isModalOpen, openModal, closeModal } = useModalHandle();
+  const [modalOpened, setmodalOpened] = useState(false);
+  const havingTodos =
+    (typeof window !== "undefined" &&
+      localStorage.getItem(LOCAL_STORAGE_KEY)) ||
+    [];
 
-  const persistedTodos = 
-    typeof window !== 'undefined' 
-    && localStorage.getItem(LOCAL_STORAGE_KEY) || [];
-  
   return (
     <div className={styles.hero}>
-      <div className={styles['hero-left']}>
-        {persistedTodos && <TodoList items={persistedTodos} />}
+      <div className={styles["hero-left"]}>
+        {havingTodos && <TodoList items={havingTodos} />}
       </div>
 
-      <div className={styles['hero-right']}>
+      <div className={styles["hero-right"]}>
         <h1>Tu bloc de notitas, más rápido que nadie 🚀</h1>
-        <div className={styles['hero-image']}>
+        <div className={styles["hero-image"]}>
           <Image
             src="/notes2.svg"
             width={600}
             height={400}
             quality={100}
-            priority 
+            priority
           />
         </div>
       </div>
 
       <Author />
-      
-      <a className={styles['hiw-cta']} onClick={openModal}>
+
+      <a className={styles["hiw-cta"]} onClick={() => setmodalOpened(true)}>
         Cómo funciona
       </a>
-      
+
       <AnimatePresence>
-        {isModalOpen && (
-          <Modal key="animatedModal" closeModal={closeModal}>
+        {modalOpened && (
+          <Modal key="animatedModal" closeModal={() => setmodalOpened(false)}>
             <HowItWorks />
           </Modal>
         )}
       </AnimatePresence>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
