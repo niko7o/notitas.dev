e8df8c609a44964304983ace073969e8fa5acb69
Pol Gubau <gubaupol@gmail.com>
2022-10-09 11:16:53 +0200

    Update TodoItem.js

--- components/TodoItem/TodoItem.js
+++ components/TodoItem/TodoItem.js
@@ -1,51 +1,42 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import { useState } from "react";
 
-import styles from './TodoItem.module.scss';
+import styles from "./TodoItem.module.scss";
+import { CgClose } from "react-icons/cg";
 
-import { hoverAnimation } from './animations';
-
-const TodoItem = ({ 
+const TodoItem = ({
   id,
   title,
-  animationVariants,
+  description,
   creationDate,
-  onRemove
+  isCompleted,
+  onRemove,
 }) => {
-  const dateOptions = { 
-    weekday: 'long', 
-    year: 'numeric', 
-    month: 'long', 
-    day: 'numeric' 
-  };
-
-  // const getFormattedDate = () => {
-  //   return creationDate.toLocaleDateString('es-ES', dateOptions)
-  // }
-
+  const [open, setOpen] = useState(false);
   return (
     <motion.div
       layout
       key={id}
       className={styles.todo}
-      variants={animationVariants}
+      // drag
+      onClick={() => setOpen(!open)}
+      // dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+      // dragElastic={0.5}
+      // dragMomentum={true}
+      // dragTransition={{ bounceStiffness: 200, bounceDamping: 20 }}
+      whileHover="hover"
+      whileTap="hover"
     >
       <span>{title}</span>
-      
-      {/* <span className={styles.date}>
-        {getFormattedDate()}
-      </span> */}
-      
-      <motion.div
-        className={styles['delete']}
-        whileHover={hoverAnimation}
-        onClick={() => onRemove(id)}
-      >
+      {open ? <p className={styles["description"]}>{description}</p> : ""}
+
+      <motion.div className={styles["delete"]} onClick={() => onRemove(id)}>
         <span>
-          ✘
+          <CgClose size={15} />
         </span>
       </motion.div>
     </motion.div>
   );
-}
+};
 
 export default TodoItem;
