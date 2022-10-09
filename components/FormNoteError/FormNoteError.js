b6bdc5a4d9fe12ff9a84bef5a250bf68d250c742
Pol Gubau <gubaupol@gmail.com>
2022-10-09 11:18:21 +0200

    Update FormNoteError.js

--- components/FormNoteError/FormNoteError.js
+++ components/FormNoteError/FormNoteError.js
@@ -1,20 +1,23 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const ERROR_COPIES = {
-  1: 'Pero escribe algo, cabesa! 🤔',
-  2: '¿Quieres escribir?',
-  3: 'Poeta de la familia.. no eres.',
-}
+  1: "Pero escribe algo, cabesa! 🤔",
+  2: "¿Quieres escribir?",
+  3: "Poeta de la familia... no eres.",
+  4: "No te rindas, sigue intentando.",
+  5: "¿No te gusta escribir?",
+  6: "Ummm...",
+};
 
 const FormNoteError = ({ errorCount }) => (
   <motion.span
     layout
-    initial={{ marginTop: '10px', opacity: 0, y: -20, color: 'crimson' }}
+    initial={{ marginTop: "10px", opacity: 0, y: -20, color: "crimson" }}
     animate={{ opacity: 1, y: 0 }}
     exit={{ opacity: 0, y: 20 }}
   >
-    {ERROR_COPIES[errorCount] || 'Escribe algo anda.'}
+    {ERROR_COPIES[errorCount] || "Necesitas ayuda? 🤔"}
   </motion.span>
-)
+);
 
 export default FormNoteError;
