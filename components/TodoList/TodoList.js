5df06caad79385e60b46205f2323127c6708ee12
Pol Gubau <gubaupol@gmail.com>
2022-10-09 11:16:40 +0200

    Update TodoList.js

--- components/TodoList/TodoList.js
+++ components/TodoList/TodoList.js
@@ -1,135 +1,236 @@
-import { useState, useRef, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import { sendEvent } from '../../utils/google-analytics';
+import Button from "../Button";
+import TodoItem from "../TodoItem";
+import TextInput from "../Inputs/TextInput";
+import SelectInput from "../Inputs/SelectInput";
 
-import Button from '../Button';
-import TodoItem from '../TodoItem';
-import TextInput from '../TextInput';
-import FormNoteError from '../FormNoteError';
+import FormNoteError from "../FormNoteError";
 
-import { LOCAL_STORAGE_KEY } from '../../utils/constants';
+import { LOCAL_STORAGE_KEY } from "../../utils/constants";
 
-import { containerVariants, itemVariants } from './animations';
-
-import styles from './TodoList.module.scss';
+import { itemVariants } from "./animations";
+import styles from "./TodoList.module.scss";
+import Categories from "./Categories/Categories";
 
 const TodoList = () => {
-  const inputRef = useRef(null);
-  const itemRef = useRef(null); // @TO-DO: implement scrollTo() after item is added
-  
-  const [todoList, setTodoList] = useState([]); // @TO-DO: use /notes endpoint
+  const [actualCategory, setActualCategory] = useState("Todas");
+  const titleRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const categoryRef = useRef(null);
+
+  const [todoList, setTodoList] = useState([
+    {
+      title: "Todas",
+      content: [],
+    },
+  ]);
+  const categoriesAbailable = [];
+  // add categories to categoriesAbailable
+  todoList.forEach((category) => {
+    categoriesAbailable.push(category.title);
+  });
+
+  const allNotitas = todoList.reduce(
+    (acc, category) => [...acc, ...category.content],
+    []
+  );
+
   const [hasError, setHasError] = useState(false);
   const [errorCount, setErrorCount] = useState(0);
 
   useEffect(() => {
-    const localTodos = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (localTodos?.length > 0) {
-      setTodoList(JSON.parse(localTodos))
+    const localNotes = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (localNotes) {
+      setTodoList(localNotes);
     }
-  }, [])
+  }, []);
 
+  // Take the focus event on change notes array
   useEffect(() => {
-    inputRef.current.focus()
-  }, [todoList])
+    titleRef.current.focus();
+  }, [todoList]);
 
   const FORM_VALIDATIONS = {
-    isNoteLongEnough: () => inputRef.current.value.length > 0
-  }
+    isNoteLongEnough: () => titleRef.current.value.length > 0,
+  };
 
   const addTodoItem = () => {
-    const inputValue = inputRef.current.value;
+    const inputValue = titleRef.current.value;
+    const descriptionValue = descriptionRef.current.value;
+    const category = categoryRef.current.value;
     const uniqueId = `${inputValue[0]}#${Date.now()}`;
     const date = new Date();
 
     const newTodo = {
       id: uniqueId,
       title: inputValue,
+      description: descriptionValue,
+      category: category,
       isCompleted: false,
-      creationDate: date
-    }
+      creationDate: date,
+    };
 
     const formValidationsPassed = FORM_VALIDATIONS.isNoteLongEnough();
-
     if (formValidationsPassed) {
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify([...todoList, newTodo ]));
-      setTodoList([...todoList, newTodo ]);
-      inputRef.current.value = '';
-      sendEvent({ category: 'TodoItem', label: 'Add', value: 'Success'})
+      //if the category is not in the list, add it to the todoList and localstorage
+      if (!categoriesAbailable.includes(category)) {
+        const newCategory = {
+          title: category,
+          content: [newTodo],
+        };
+        setTodoList([...todoList, newCategory]);
+        localStorage.setItem(
+          LOCAL_STORAGE_KEY,
+          JSON.stringify([...todoList, newCategory])
+        );
+      } else {
+        //if the category is in the list, add the note to the category and localstorage
+        const newTodoList = todoList.map((category) => {
+          if (category.title === newTodo.category) {
+            return {
+              ...category,
+              content: [...category.content, newTodo],
+            };
+          } else {
+            return category;
+          }
+        });
+        setTodoList(newTodoList);
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTodoList));
+      }
+
+      //move the actual category to the one that has the new note
+      setActualCategory(category);
+
+      titleRef.current.value = "";
+      descriptionRef.current.value = "";
     } else {
-      showErrorForSeconds(3);
-      setErrorCount(errorCount + 1)
-      sendEvent({ category: 'TodoItem', label: 'Add', value: 'Error'})
+      showErrorForSeconds(5);
+      setErrorCount(errorCount + 1);
     }
-  }
+  };
 
-  const removeTodoItem = idToRemove => {
-    const filteredTodos = todoList.filter(item => item.id !== idToRemove)
-    setTodoList(filteredTodos);
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(filteredTodos));
-    sendEvent({ category: 'TodoItem', label: 'Remove', value: 'Success'})
-  }
+  const removeTodoItem = (idToRemove) => {
+    // we need to delete the note from the category and localstorage
+    const newTodoList = todoList.map((category) => {
+      return {
+        ...category,
+        content: category.content.filter((note) => note.id !== idToRemove),
+      };
+    });
 
-  const handleKeyPress = event => {
-    // @TO-DO: Implement keyboard navigation/shortcuts (arrows + del)
-    if (event.key === 'Enter') {
+    //if its category is empty, it is deleted unless is the "Todas" category
+    const newTodoListFiltered = newTodoList.filter(
+      (category) => category.content.length > 0 || category.title === "Todas"
+    );
+    //if some category got deleted, the actual category is set to "Todas"
+    if (newTodoListFiltered.length < newTodoList.length) {
+      setActualCategory("Todas");
+    }
+
+    setTodoList(newTodoListFiltered);
+    localStorage.setItem(
+      LOCAL_STORAGE_KEY,
+      JSON.stringify(newTodoListFiltered)
+    );
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
       addTodoItem();
     }
-  }
+  };
 
-  const showErrorForSeconds = seconds => {
+  const showErrorForSeconds = (seconds) => {
     setHasError(true);
     setTimeout(() => {
       setHasError(false);
     }, seconds * 1000);
-  }
+  };
 
   return (
-    <div className={styles['todo-list']}>
-      <AnimatePresence>
-        <motion.div
-          key="todoContainer"
-          variants={containerVariants}
-          initial="entering"
-          animate="active"
-          exit="exiting"
-        >
-          {todoList.length > 0 && todoList.map(item => (
-            <TodoItem
-              id={item.id}
-              key={item.id}
-              title={item.title}
-              creationDate={item.creationDate}
-              isCompleted={item.isCompleted}
-              onRemove={removeTodoItem}
-              animationVariants={itemVariants}
-            />
-          ))}
-        </motion.div>
+    <div className={styles["todo-list"]}>
+      <div className={styles["todo-list__header"]}>
+        <h1 className={styles["todo-list__title"]}>
+          Notitas en {actualCategory}
+        </h1>
+        <Categories
+          todoList={todoList}
+          setTodoList={setTodoList}
+          categoriesAbailable={categoriesAbailable}
+          setActualCategory={setActualCategory}
+          actualCategory={actualCategory}
+        />
+      </div>
+      <motion.div
+        className={styles["todo-list-container"]}
+        key="todoContainer"
+        initial="entering"
+        exit="exiting"
+      >
+        {allNotitas.length === 0
+          ? ""
+          : actualCategory === "Todas"
+          ? allNotitas.map((todo) => {
+              return (
+                <div key={todo.id}>
+                  <>
+                    <TodoItem
+                      id={todo.id}
+                      title={todo.title}
+                      description={todo.description}
+                      creationDate={todo.creationDate}
+                      isCompleted={todo.isCompleted}
+                      onRemove={removeTodoItem}
+                      animationVariants={itemVariants}
+                    />
+                  </>
+                </div>
+              );
+            })
+          : //show the notes of the category selected
+            todoList
+              .filter((category) => category.title === actualCategory)
+              .map((category) => {
+                return category.content.map((todo) => {
+                  return (
+                    <div key={todo.id}>
+                      <>
+                        <TodoItem
+                          id={todo.id}
+                          title={todo.title}
+                          description={todo.description}
+                          creationDate={todo.creationDate}
+                          isCompleted={todo.isCompleted}
+                          onRemove={removeTodoItem}
+                          animationVariants={itemVariants}
+                        />
+                      </>
+                    </div>
+                  );
+                });
+              })}
+      </motion.div>
+      <motion.div key="todoFormInput" layout className={styles["form"]}>
+        <TextInput
+          nodeRef={titleRef}
+          onKeyPress={handleKeyPress}
+          placeholder="Título"
+        />
+        <TextInput
+          nodeRef={descriptionRef}
+          onKeyPress={handleKeyPress}
+          placeholder="Descripción (opcional)"
+        />
+        <SelectInput nodeRef={categoryRef} categories={categoriesAbailable} />
 
-        <motion.div key="todoFormInput" layout className={styles['form']}>
-          <TextInput
-            nodeRef={inputRef}
-            onKeyPress={handleKeyPress}
-            placeholder="Escribe aquí.."
-          />
-          <Button onClick={addTodoItem} title="Añadir nota (Enter)" /> 
-          {hasError && <FormNoteError errorCount={errorCount}/>}
-        </motion.div>
-      </AnimatePresence>
+        <Button onClick={addTodoItem} title="Añadir nota (Enter)" />
+        {hasError && <FormNoteError errorCount={errorCount} />}
+      </motion.div>
     </div>
   );
-}
-
-export async function getServerSideProps(context) {
-  const res = await fetch(`http://localhost:3000/api/notes`)
-  const notes = await res.json()
-
-  return {
-    props: {
-      notes
-    }
-  }
-}
+};
 
 export default TodoList;
